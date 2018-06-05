07249499a87c16ec8d27e3a493a7de773f691b1c
MadCake <wdiabloster@gmail.com>
2018-06-05 12:36:42 +0700

    Add multi-transaction support

--- trust-core/src/main/java/trust/core/entity/Message.java
+++ trust-core/src/main/java/trust/core/entity/Message.java
@@ -7,19 +7,26 @@ public class Message implements Parcelable {
 
     public final String value;
     public final boolean isPersonal;
+    public final long leafPosition;
 
     public Message(String value) {
         this(value, false);
     }
 
     public Message(String value, boolean isPersonal) {
+        this(value, isPersonal, 0);
+    }
+
+    public Message(String value, boolean isPersonal, long leafPosition) {
         this.value = value;
         this.isPersonal = isPersonal;
+        this.leafPosition = leafPosition;
     }
 
     protected Message(Parcel in) {
         value = in.readString();
         isPersonal = in.readByte() != 0;
+        leafPosition = in.readLong();
     }
 
     public static final Creator<Message> CREATOR = new Creator<Message>() {
@@ -43,5 +50,6 @@ public class Message implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(value);
         dest.writeByte((byte) (isPersonal ? 1 : 0));
+        dest.writeLong(leafPosition);
     }
 }
